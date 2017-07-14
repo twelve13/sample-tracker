abd6fd7bc75cb8336ddf7e4b06f9f6c3d48fec75
Caroline Chen <carolinecaroline@gmail.com>
2017-07-14 13:10:02 -0400

    seed database

---
+++ db/schema.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/sample-tracker");
+
+mongoose.connection.on("error", err => {
+	console.log(err);
+});
+
+mongoose.connection.once("open", () => {
+	console.log("database has been connected");
+});
+
+const ExtractionSchema = new mongoose.Schema({
+	name: String,
+	goal_date: String,
+	analyst: String,
+	bbp_added: Boolean,
+	bbp_date: {type: Date, default: Date.now},
+	extracted: Boolean,
+	extracted_date: {type: Date, default: Date.now}
+});
+
+const SampleSchema = new mongoose.Schema({
+	name: String,
+	notes: String,
+	strs: Boolean,
+	mito: Boolean,
+	priority: Boolean,
+	analyst: String,
+	cleaned: Boolean,
+	cleaned_date: {type: Date, default: Date.now},
+	sampled: Boolean,
+	sampled_date: {type: Date, default: Date.now},
+	extraction: {type: mongoose.Schema.ObjectId, ref: "Extraction"}
+});
+
+var Extraction = mongoose.model("Extraction", ExtractionSchema);
+var Sample = mongoose.model("Sample", SampleSchema);
+
+module.exports = {
+	Extraction: Extraction,
+	Sample: Sample
+};
---
+++ db/seeds.js
@@ -0,0 +1,111 @@
+const Schema = require("./schema.js");
+
+const Extraction = Schema.Extraction;
+const Sample = Schema.Sample;
+
+Extraction.remove({}, err => {
+	if(err){
+		console.log(err);
+	}
+});
+
+Sample.remove({}, err => {
+	if(err){
+		console.log(err);
+	}
+});
+
+const ext1 = new Extraction({
+	name: "071417-EXT1",
+	goal_date: "7/17/17",
+	analyst: "CC",
+	bbp_added: false,
+	extracted: false,
+});
+
+const ext2 = new Extraction({
+	name: "071417-EXT2",
+	goal_date: "7/17/17",
+	analyst: "ABC",
+	bbp_added: false,
+	extracted: false
+});
+
+
+const sample1 = new Sample({
+	name: "ABC1701-0001-E01",
+	notes: "clean well",
+	strs: true,
+	mito: true,
+	priority: false,
+	analyst: "CC",
+	cleaned: false,
+	sampled: false
+});
+
+const sample2 = new Sample({
+	name: "ABC1701-0002-E01",
+	notes: "sample the teeth",
+	strs: true,
+	mito: true,
+	priority: false,
+	analyst: "CC",
+	cleaned: false,
+	sampled: false
+});
+
+const sample3 = new Sample({
+	name: "ABC1701-0003-E01",
+	strs: true,
+	mito: false,
+	priority: false,
+	analyst: "CC",
+	cleaned: false,
+	sampled: false
+});
+
+const sample4 = new Sample({
+	name: "DEF1701-0001-E01",
+	notes: "clean well",
+	strs: true,
+	mito: true,
+	priority: false,
+	analyst: "XYZ",
+	cleaned: false,
+	sampled: false
+});
+
+const sample5 = new Sample({
+	name: "DEF1701-0002-E01",
+	notes: "sample 5g",
+	strs: false,
+	mito: true,
+	priority: true,
+	analyst: "XYZ",
+	cleaned: false,
+	sampled: false
+});
+
+const samples = [sample1, sample2, sample3, sample4, sample5];
+
+samples.forEach((sample, i) => {
+	sample.save((err, sample) => {
+		if(err){
+			console.log(err)
+		} else {
+			console.log(`${sample} was saved to the database`)
+		}
+	});
+});
+
+const extractions = [ext1, ext2];
+
+extractions.forEach((extraction, i) => {
+	extraction.save((err, extraction) => {
+		if(err){
+			console.log(err)
+		} else {
+			console.log(`${extraction} was saved to the database`)
+		}
+	});
+});
