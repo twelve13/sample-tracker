4e5cc69ab0499149a95cb8dc77056e00bd341758
Caroline Chen <carolinecaroline@gmail.com>
2017-07-28 16:51:08 -0400

    found typo in index js

--- index.js
+++ index.js
@@ -117,6 +117,6 @@ app.get("/*", (req, res) => {
 	res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.listen(app.get("port", () => {
+app.listen(app.get("port"), () => {
 	console.log("it's alive!")
 });
